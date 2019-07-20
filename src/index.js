c479b9df5af3fb705722fdfc6daee5354a21bc97
johnnythrash <chris.garofolo@gmail.com>
2019-07-20 05:12:49 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import 'phaser';
+
+
+
+const config = {
+  type: Phaser.Auto,
+  width: 490,
+  height: 630,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y:300 },
+      debug: false
+    }
+  },
+  scene: []
+};
+
+// game
+const game = new Phaser.Game(config);
